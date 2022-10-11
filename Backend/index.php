e41abd56c460562cfeab7df29a2d23f669d00d21
(kitcloud) Tobias Kjær Boss Gjerstrup <vl3b@kk.dk>
2022-10-11 14:08:50 +0200

    frontend can now talk to backend succesfully

--- Backend/index.php
+++ Backend/index.php
@@ -1,11 +1,18 @@
 <?php
-ini_set('display_errors','Off');
+header('Access-Control-Allow-Origin: http://localhost:4200');
+ini_set('display_errors', 'Off');
 
 $queries = array();
 parse_str($_SERVER['QUERY_STRING'], $queries);
 
-if (isset($queries['ID'])) {
-    echo("you searched for an ID!");
-} else {
-    echo("invalid query");
+
+switch ($queries['ID']) {
+    case 'JSON':
+        $array = array("test" => 1, "test2" => 2, "test3" => 2, "test4" => 2, "test5" => 2, "test6" => 2, "test7" => 2, "test8" => 2);
+        $json = json_encode($array);
+        echo($json);
+        break;
+    default:
+        echo ("Invalid ID");
+        break;
 }
--- Frontend/my-app/src/app/app.component.ts
+++ Frontend/my-app/src/app/app.component.ts
@@ -1,10 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'my-app';
+  private data: any = [];
+  constructor(private http: HttpClient) {
+
+  }
+
+  getData(){
+    const url ='http://localhost:8000/?ID=JSON'
+    this.http.get(url).subscribe((res)=>{
+      this.data = res
+      console.log(this.data)
+    })
+  }
 }
